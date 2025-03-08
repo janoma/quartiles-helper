45ccb7107210d123b497aef8af7ab5d5c5c72587
Alejandro Mallea <janoma@gmail.com>
2025-03-08 08:48:12 -0800

    use prod path

--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -9,10 +9,14 @@ export default async function getDictionarySingleton(): Promise<Dictionary> {
     private static instance: Dictionary | undefined;
 
     public static async getInstance(): Promise<Dictionary> {
+      const isProduction = process.env.NODE_ENV === "production";
+
       if (!DictionarySingleton.instance) {
         console.log("CWD", process.cwd());
         console.log("NODE_ENV", process.env.NODE_ENV);
-        DictionarySingleton.instance = new Dictionary("en-wordnet");
+        DictionarySingleton.instance = new Dictionary(
+          isProduction ? ".next/server/en-wordnet" : "en-wordnet",
+        );
         await DictionarySingleton.instance.init();
       }
       return DictionarySingleton.instance;
