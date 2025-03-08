a6b130d50ef2a9a2af0eb37599be49d019a8979d
Alejandro Mallea <janoma@gmail.com>
2025-03-07 20:20:38 -0800

    use server only and try to read wordnet from lib

--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -1,11 +1,13 @@
 import Dictionary from "en-dictionary";
 
+import "server-only";
+
 export class DictionarySingleton {
   private static instance: Dictionary | undefined;
 
   public static async getInstance(): Promise<Dictionary> {
     if (!DictionarySingleton.instance) {
-      DictionarySingleton.instance = new Dictionary("en-wordnet");
+      DictionarySingleton.instance = new Dictionary("lib/en-wordnet");
       await DictionarySingleton.instance.init();
     }
     return DictionarySingleton.instance;
