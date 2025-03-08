0a47ad82e275ed71052814706c04958906c34fef
Alejandro Mallea <janoma@gmail.com>
2025-03-08 08:32:35 -0800

    use copy webpack plugin to output wordnet

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { withVercelToolbar } from "@vercel/toolbar/plugins/next";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 
 /** @type {import("next").NextConfig} */
-const config = withVercelToolbar()({});
+const config = withVercelToolbar()({
+  experimental: {
+    turbo: {},
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: "en-wordnet/",
+              to: "en-wordnet/",
+            },
+          ],
+        }),
+      );
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return config;
+  },
+});
 
 export default config;
