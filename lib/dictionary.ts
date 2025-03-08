2b8915ce716bfd78bb357707e2b056ef86c99fa0
Alejandro Mallea <janoma@gmail.com>
2025-03-08 09:58:51 -0800

    more logs

--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -11,12 +11,12 @@ export default async function getDictionarySingleton(): Promise<Dictionary> {
 
     public static async getInstance(): Promise<Dictionary> {
       const isProduction = process.env.NODE_ENV === "production";
-      const files = await promises.readdir(`${process.cwd()}/.next`);
+      let files = await promises.readdir(`${process.cwd()}`);
+      console.log("CWD files", files);
+      files = await promises.readdir(`${process.cwd()}/.next`);
       console.log(".next", files);
-      const filesInServer = await promises.readdir(
-        `${process.cwd()}/.next/server`,
-      );
-      console.log(".next/server", filesInServer);
+      files = await promises.readdir(`${process.cwd()}/.next/server`);
+      console.log(".next/server", files);
 
       if (!DictionarySingleton.instance) {
         console.log("CWD", process.cwd());
