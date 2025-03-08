dbd049aef145666d674eeaea242ddc43c7e08c77
Alejandro Mallea <janoma@gmail.com>
2025-03-08 11:10:51 -0800

    remove console logging and document config

--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import Dictionary from "en-dictionary";
-import { promises } from "fs";
 
 import "server-only";
 
@@ -10,16 +9,7 @@ export default async function getDictionarySingleton(): Promise<Dictionary> {
     private static instance: Dictionary | undefined;
 
     public static async getInstance(): Promise<Dictionary> {
-      let files = await promises.readdir(`${process.cwd()}`);
-      console.log("CWD files", files);
-      files = await promises.readdir(`${process.cwd()}/.next`);
-      console.log(".next", files);
-      files = await promises.readdir(`${process.cwd()}/.next/server`);
-      console.log(".next/server", files);
-
       if (!DictionarySingleton.instance) {
-        console.log("CWD", process.cwd());
-        console.log("NODE_ENV", process.env.NODE_ENV);
         DictionarySingleton.instance = new Dictionary("en-wordnet");
         await DictionarySingleton.instance.init();
       }
--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ const config = withVercelToolbar()({
   },
   output: "standalone",
   outputFileTracingIncludes: {
+    /**
+     * This will copy the en-wordnet directory to the standalone directory,
+     * provided that output: "standalone" is set.
+     * This is necessary because en-wordnet is not imported by any file,
+     * just read dynamically in runtime. Without this, the dictionary lookup
+     * works locally but not in the production server.
+     */
     "*": ["en-wordnet/**/*"],
   },
 });
